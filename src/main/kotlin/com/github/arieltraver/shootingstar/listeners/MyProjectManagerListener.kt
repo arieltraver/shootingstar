da0c64bc0fae5c12a3f8a9dc823c1d87c32c09a5
GitHub Action <action@github.com>
2021-03-21 15:40:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/arieltraver/shootingstar/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/arieltraver/shootingstar/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.arieltraver.shootingstar.listeners
 
+import com.github.arieltraver.shootingstar.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/arieltraver/shootingstar/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/arieltraver/shootingstar/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.arieltraver.shootingstar.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.arieltraver.shootingstar.MyBundle
 
 class MyApplicationService {
 
